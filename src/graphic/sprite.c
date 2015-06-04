9e793568d495b30e00902cfa5f996dadfb93efcb
Voldie <voldemor2009@hotmail.com>
2015-06-04 16:31:32 +0200

    added shader and sprite code

---
+++ src/graphic/sprite.c
@@ -0,0 +1,12 @@
+#include"graphic/sprite.h"
+
+
+DECLSPEC ExSpriteBatch* ExCreateSpriteBatch(void){
+	ExSpriteBatch* batch;
+
+	return batch;
+}
+DECLSPEC int ELTAPIENTRY ExReleaseSpriteBatch(ExSpriteBatch* spritebatch){
+
+	return 1;
+}
