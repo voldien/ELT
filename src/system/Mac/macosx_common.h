be9579c5352eb56b68c013e0e02b45002aa56577
Voldie <voldemor2009@hotmail.com>
2015-11-12 11:25:13 +0100

    Removed useless files.

--- src/system/Mac/macosx_common.h
+++
@@ -1,9 +0,0 @@
-/*
-===========================================================================
-Copyright (C) 2014 BroodCity Software, Inc. Voldemor2009@hotmail.com
-
-*/
-#ifndef MAC_OS_X_COMMON_H
-#define MAC_OS_X_COMMON_H
-
-#endif
