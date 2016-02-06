061db4e4af830d3412ec6cb0e60f25ae9f8b71e5
Voldie <voldemor2009@hotmail.com>
2016-02-06 09:34:54 +0100

    Started implementing camera controller.

--- samples/common/common.h
+++ samples/common/common.h
@@ -7,10 +7,18 @@
 extern "C"{
 #endif
 
+typedef struct camera_controller{
+	vec3_t position;
+	quad_t rotation;
+	vec3_t direction;
+	vec4x4_t proj;
+}CameraController ;
+
 
 DECLSPEC extern void* ExCreateCharacterController(const vec3_t position);
 
-DECLSPEC extern void ExUpdateCharacterController(void* charactercontroller);
+
+DECLSPEC extern void ExUpdateCharacterController(CameraController* charactercontroller);
 
 
 DECLSPEC extern void readArgument(int argc, char** argv);
