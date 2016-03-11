c882121ea8a295d01b7bfc3aa858fab9e06d5335
Voldie <voldemor2009@hotmail.com>
2016-03-11 15:13:34 +0100

    Minor event improvement, started relocating event implementation to its corresponding platform source file.

--- src/system/unix/unix_event.c
+++ src/system/unix/unix_event.c
@@ -1,1 +1,148 @@
 #include"system/elt_event.h"
+#	include"system/unix/unix_win.h"
+#	include<X11/X.h>
+#	include<X11/Xlib.h>
+
+Int32 ExPollEvent(ExEvent* event){
+
+	XEvent msg;
+	if(XPending(display)){
+		XNextEvent(display,&msg);
+		event->event = 0;
+
+		switch(msg.type){
+
+		case KeyPress:{
+		    event->event |= EX_EVENT_KEY;
+		    event->event |= EX_EVENT_KEY_PRESSED;
+		    event->key.code = XLookupKeysym(&msg.xkey,0);
+		    event->mouse.x = msg.xkey.x;
+		    event->mouse.y = msg.xkey.y;
+		    event->key.alt = msg.xkey.state & Mod1Mask;
+		    event->key.ctrl = msg.xkey.state & ControlMask;
+		    event->key.shift = msg.xkey.state & ShiftMask;
+		    event->key.system = msg.xkey.state & Mod1Mask;
+
+        }break;
+		case KeyRelease:{
+		    event->event |= EX_EVENT_KEY;
+		    event->event |= EX_EVENT_KEY_RELEASE;
+		    event->key.code = XLookupKeysym(&msg.xkey,0);
+		    event->mouse.x = msg.xkey.x;
+		    event->mouse.y = msg.xkey.y;
+		    event->key.alt = msg.xkey.state & Mod1Mask;
+		    event->key.ctrl = msg.xkey.state & ControlMask;
+		    event->key.shift = msg.xkey.state & ShiftMask;
+		    event->key.system = msg.xkey.state & Mod1Mask;
+
+		}break;
+		case ButtonPress:{
+		    event->event |= EX_EVENT_MOUSE;
+		    event->button.button = msg.xbutton.button;
+		    event->mouse.x = msg.xkey.x;
+		    event->mouse.y = msg.xkey.y;
+        }break;
+
+		case ButtonRelease:{
+		    event->event |= EX_EVENT_MOUSE;
+		    event->button.button = msg.xbutton.button;
+		    event->mouse.x = msg.xkey.x;
+		    event->mouse.y = msg.xkey.y;
+
+		}break;
+		case MotionNotify:{
+			event->event |= EX_EVENT_MOUSE_MOTION;
+			event->motion.xdelta =  msg.xmotion.x - msg.xkey.x;
+			event->motion.ydelta =  msg.xmotion.y - msg.xkey.y;
+			event->motion.x = msg.xmotion.x;
+			event->motion.y = msg.xmotion.y;
+
+		}break;
+		case LeaveNotify:
+			event->event |= EX_EVENT_ON_UNFOCUSE;
+			break;
+		case EnterNotify:
+			event->event |= EX_EVENT_ON_FOCUSE;
+			break;
+		case GravityNotify:
+			event->event |= EX_EVENT_WINDOW_MOVE;
+			break;
+		case ResizeRequest:{
+            event->event |= EX_EVENT_SIZE;
+            event->size.width = msg.xresizerequest.width;
+            event->size.height = msg.xresizerequest.height;
+		}break;
+		case Expose:{
+			event->event |= EX_EVENT_EXPOSE;
+			event->size.width = msg.xexpose.width;
+			event->size.height = msg.xexpose.height;
+            msg.xexpose.x;
+		}break;
+		case ClientMessage:{
+            //event->event |= EX_EVENT_SIZE;
+            /*
+            if((Atom)msg.xclient.data.l[0] == wm_delete_window){
+
+            }
+            */
+
+		}break;
+		case  VisibilityNotify:{
+
+
+		}break;
+		case ConfigureNotify:{
+            //event->event |= EX_EVENT_SIZE;
+            //event->size.width =  msg.xconfigure.width;
+            //event->size.height = msg.xconfigure.height;
+		}break;
+		case DestroyNotify:{
+			event->event |= EX_EVENT_WINDOW_DESTROYED;
+			event->destroy.window = msg.xdestroywindow.event;
+
+		}break;
+		case FocusIn:
+			event->event |= EX_EVENT_ON_FOCUSE;
+			break;
+		case FocusOut:
+			event->event |= EX_EVENT_ON_UNFOCUSE;
+			break;
+		case KeymapNotify:
+            XRefreshKeyboardMapping(&msg.xmapping);
+        break;
+		case LASTEvent:
+			event->event = 0;
+			return FALSE;
+		default:
+			event->event |= msg.type;
+			break;
+		}
+		event->time = ExGetTicks();
+		event->window = msg.xany.window;
+		return TRUE;
+	}else {/*XSync(display,TRUE);*/ return FALSE;}
+}
+
+Int32 ExPollWindowEvent(ExWin window, ExWindowEvent* event){
+	XEvent msg;
+    if(XPending(display)){
+    	XWindowEvent(display, window, 0, &msg);
+		switch(msg.type){
+
+
+
+
+		}
+
+	}
+    else{
+    	/*XSync(display,TRUE);*/ return FALSE;
+    }
+
+	return TRUE;
+}
+
+
+Int32 ExForwardEvent(Uint32 event, ExHandle data, Uint32 size){
+	XSendEvent(display,0,True,event,data);
+}
