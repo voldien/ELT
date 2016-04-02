5bc3658e6e56b7efa19e818431981a64b2972ef5
Voldie <voldemor2009@hotmail.com>
2016-04-02 14:14:41 +0200

    Added filesystem header and added additional log functions.

--- include/system/elt_filesystem.h
+++ include/system/elt_filesystem.h
@@ -1,6 +1,6 @@
-/*
+/**
     ELT (Engine Library Toolkit) is a multi platform engine toolkit
-    Copyright (C) 2014  Valdemar Lindberg
+    Copyright (C) 2016  Valdemar Lindberg
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,44 +16,36 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-#ifndef _ELT_LOG_H_
-#define _ELT_LOG_H_ 1
+#ifndef _ELT_FILE_SYSTEM_H_
+#define _ELT_FILE_SYSTEM_H_ 1
 #include"./../EngineAssembly.h"
-extern FILE* m_file_log;
 
-
-/*
- *
- */
-extern ELTDECLSPEC void ELTAPIENTRY ExLog(const ExChar* format,...);
+#ifdef __cplusplus	/*	C++ Environment	*/
+extern "C"{
+#endif
 
 /*
  *
+ *	@Return
  */
-extern ELTDECLSPEC void ELTAPIENTRY ExErrorLog(const ExChar* format,...);
+extern ELTDECLSPEC ExBoolean ELTAPIENTRY ExCreateRamDisk(const ExChar* cdirectory, unsigned int nBytes);
 
 /*
  *
+ *	@Return
  */
-extern ELTDECLSPEC void ELTAPIENTRY ExLogWarning(const ExChar* format,...);
+extern ELTDECLSPEC ExBoolean ELTAPIENTRY ExMount(const ExChar* source, const ExChar* target, const ExChar* filesystemtype, unsigned long mountflags, const void *data);
 
 /*
  *
+ *	@Return
  */
-extern ELTDECLSPEC void ELTAPIENTRY ExLogCritical(const ExChar* format,...);
-
-/*
- *
- */
-extern ELTDECLSPEC void ELTAPIENTRY ExLogInfo(const ExChar* format,...);
-
-
-/*
- *
- */
-extern ELTDECLSPEC FILE* ELTAPIENTRY ExDupFile(const int _fd);
+extern ELTDECLSPEC ExBoolean ELTAPIENTRY ExUMount(const ExChar* cdirectory);
 
 
 
+#ifdef __cplusplus	/*	C++ Environment	*/
+}
+#endif
 
 #endif
