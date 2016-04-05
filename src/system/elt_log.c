b2355d1b056de04bb3e7d6da8ef8b8c3991303b3
Voldie <voldemor2009@hotmail.com>
2016-04-05 10:40:36 +0200

    Added additional logging functions, remake still in design process.

--- src/system/elt_log.c
+++ src/system/elt_log.c
@@ -1,2 +1,30 @@
 #include"system/elt_log.h"
 FILE* m_file_log = NULL;
+
+
+void ExLog(const ExChar* format,...){
+	va_list va;
+	//vfprintf(stdout, format, va_arg());
+}
+
+void ExErrorLog(const ExChar* format,...){
+
+}
+
+void ExLogWarning(const ExChar* format,...){
+
+}
+
+void ExLogCritical(const ExChar* format,...){
+
+}
+
+void ExLogInfo(const ExChar* format,...){
+
+}
+
+
+
+FILE* ExDupFile(const int _fd){
+
+}
