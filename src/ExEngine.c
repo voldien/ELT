4f55293a10be7ee0adc97c4285850cb546b91aeb
Voldie <voldemor2009@hotmail.com>
2016-05-30 11:43:53 +0200

    Redirected ExInit function to their corresponding platform directory.

--- src/ExEngine.c
+++ src/ExEngine.c
@@ -87,6 +87,8 @@ _In_  HINSTANCE hinstDLL,
 
 
 
+
+
 /*
  *	High accuracy timer.
  */
@@ -95,319 +97,13 @@ extern Uint64 eltTickTime;
 /*
  *
  */
-unsigned long int engineflag = 0;
+extern unsigned long int engineflag;
 #define ELT_DEINIT ((unsigned long int)(-1))
 
 /*
  *
  */
 void* xcbConnection;
-
-
-ERESULT ExInit(Enum engineFlag){
-	ERESULT result = E_OK;
-	ExHandle hmodule;
-	Int32 hConHandle;
-	Long lStdHandle;
-
-
-	/*	if all is already initilated return !	*/
-    if(engineflag & ELT_INIT_EVERYTHING)
-        return 2;
-	engineflag = engineflag & ~ELT_DEINIT;
-
-
-    /*	*/
-#ifdef EX_DEBUG || (EX_ENGINE_VERSION_MAJOR <= 0)
-	#if defined(EX_VC) || defined(EX_WINDOWS)
-	// debug shell
-	// redirect unbuffered STDOUT to the console
-	lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-	////
-	hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-	/**/
-	m_file_log = fopen("EngineExDevLog.txt", "w+" );
-	FILE* fopen;
-	/**/
-	if(dup2(1,(fopen = fdopen(4,"w+"))) == -1)
-        fprintf(stderr,"error");
-    /**/
-    /*  cause fork to fail somehow*/
-	*stdout = *m_file_log;
-	setvbuf(fopen, NULL, _IONBF, 0 );
-	/**
-        visual studio debug
-	*/
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_ALLOC_MEM_DF);
-	_CrtSetReportMode(_CRT_ASSERT , _CRTDBG_MODE_FILE);
-	_CrtSetReportFile(_CRT_ASSERT , _CRTDBG_FILE_STDERR);
-	#elif defined(EX_UNIX)
-
-
-        mtrace();		/**/
-
-
-	#endif
-#endif
-
-
-	/*	enable loggint */
-	m_file_log = fopen("EngineExDevLog.txt", "w+" );
-	if(dup2(stdout,m_file_log) == -1)
-        fprintf(stderr,"error");
-    dup2(stdout,stderr);  //redirects stderr to stdout below this line.
-
-
-#if defined(EX_WINDOWS)
-
-
-#elif defined(EX_LINUX)
-
-    /*	Create Connection with Display Server.	*/
-    display = XOpenDisplay(getenv("DISPLAY"));
-    if(!display){
-        ExError("couldn't open Display\n");
-        abort();
-    }
-
-
-
-#elif defined(EX_APPLE)
-
-#elif defined(EX_MAC)
-
-#elif defined(EX_ANDROID)
-
-#endif
-
-
-	/*	Initialize sub system	*/
-	ExInitSubSystem(engineFlag);
-
-
-	/*	initialize error handler.	*/
-	if(engineFlag & ELT_INIT_DEBUG){
-		if(!(result = ExInitErrorHandler())){
-			ExError(EX_TEXT("Failed to initialize error handler."));
-		}
-	}
-
-	engineflag |= engineFlag;
-
-	/* release resources even if application exit unexpected or exit without calling ExShutDown */
-	atexit(ExShutDown);
-
-	return result;
-}
-
-ERESULT ExInitSubSystem(Uint32 engineflag){
-	ERESULT hr = 0;
-	ExHandle hmodule;
-
-	/**/
-	engineflag = engineflag & ~ELT_DEINIT;
-
-
-
-	if(ELT_INIT_VIDEO & engineflag){
-        #ifdef EX_WINDOWS
-		/* Load OpenGL library*/
-		ExIsWinError(!(hmodule = LoadLibrary(EX_TEXT("OpenGL32.dll"))));
-        #elif defined(EX_LINUX)
-
-        #elif defined(EX_ANDROID)
-
-        #endif
-
-	}
-	if(ELT_INIT_JOYSTICK & engineflag){
-
-
-	}
-	if(ELT_INIT_AUDIO & engineflag){
-		//ExAudioInit(0);
-
-
-	}
-	if(ELT_INIT_GAMECONTROLLER & engineflag){
-#ifdef EX_WINDOWS
-		ExLoadLibrary(EX_TEXT("Xinput.dll"));
-		//ExInitGameController();
-#elif defined(EX_LINUX)
-
-#elif defined(EX_ANDROID)
-
-#endif
-	}
-	if(ELT_INIT_EVENTS & engineflag){
-#if defined(EX_WINDOWS)
-
-#elif defined(EX_LINUX)
-	    /*		enable X events	*/
-		XAllowEvents(display , SyncBoth,CurrentTime);
-#endif
-	}
-	if(ELT_INIT_TIMER & engineflag){
-		eltTickTime = ExCurrentTime();
-	}
-	if(ELT_INIT_NET & engineflag){
-
-	    #ifdef EX_LINUX
-        ExLoadLibrary("");
-        #elif defined(EX_ANDROID)
-
-        #elif defined(EX_WINDOWS)
-        if(!ExIsModuleLoaded(EX_TEXT("WS2_32.dll")))
-        	ExLoadLibrary(EX_TEXT("WS2_32.dll"));
-        #endif
-
-	}
-	return hr;
-}
-
-
-void ExQuitSubSytem(Uint32 engineflag){
-
-	if(ELT_INIT_TIMER & engineflag){
-        #ifdef EX_WINDOWS   // EX_WINDOWS
-
-        #elif defined(EX_LINUX)
-
-        #endif
-	}
-	if(ELT_INIT_AUDIO & engineflag){
-        #ifdef EX_WINDOWS
-        ExUnLoadObject(ExGetFileModule(EX_TEXT("")));
-        #elif defined(EX_LINUX)
-        ExUnLoadObject(ExGetFileModule(EX_TEXT("libasound.so")));
-        #endif
-	}
-	if(ELT_INIT_JOYSTICK & engineflag){
-        ExJoyStickClose(0);
-        ExJoyStickClose(1);
-        ExJoyStickClose(2);
-        ExJoyStickClose(3);
-		//ExJoyStickShutDown();
-	}
-	if(ELT_INIT_GAMECONTROLLER & engineflag){
-        #ifdef EX_WINDOWS
-            ExUnLoadObject(ExGetFileModule(EX_TEXT("Xinput.dll")));
-        #endif // EX_WINDOWS
-	}
-	if(ELT_INIT_NET & engineflag){
-#ifdef EX_WINDOWS
-		ExUnLoadObject(ExGetFileModule(EX_TEXT("Wininet.dll")));
-#elif defined(EX_LINUX)
-
-#endif
-	}
-
-	if(ELT_INIT_VIDEO & engineflag){
-
-	}
-
-	if(ELT_INIT_NET & engineflag){
-	    #ifdef EX_LINUX
-        ExLoadLibrary("");
-        #elif defined(EX_WINDOWS)
-    	if(ExIsModuleLoaded(EX_TEXT("Ws2_32.dll")))
-    		WSACleanup();
-        #endif
-	}
-
-
-
-
-}
-void ExShutDown(void){
-	if(engineflag & ELT_DEINIT)
-		return;
-
-#ifdef EX_LINUX
-    struct mallinfo mi;
-#endif
-
-	ExQuitSubSytem(ELT_INIT_EVERYTHING);
-
-
-	if(ExGetCurrentOpenGLContext())
-		ExDestroyGLContext(ExGetCurrentGLDrawable(), ExGetCurrentOpenGLContext());
-
-#if !(defined(EX_ANDROID) || defined(DONT_SUPPORT_OPENCL))
-	ExDestroyCLContext(ExGetCurrentCLContext());
-#endif
-
-
-
-#if defined(EX_INCLUDE_DIRECTX)
-	ExReleaseDirectX();
-#endif
-
-#ifdef EX_WINDOWS
-	DEVMODE d = {0};
-	Int32 display;
-
-	ExUnRegisterClasses();
-
-	// restore screen
-	display = ChangeDisplaySettings(&d, NULL);
-	FreeLibrary(GetModuleHandle(NULL));
-
-#elif defined(EX_LINUX)
-
-
-
-	if(eglGetCurrentDisplay())
-		eglTerminate(eglGetCurrentDisplay());
-
-	if(xcbConnection)
-		XCloseDisplay(xcbConnection);
-	XSync(display,True);
-	if(display)
-		XFlush(display);
-    if(display){
-    	XCloseDisplay(display);
-    	display = NULL;
-    }
-
-
-#elif defined(EX_APPLE)
-
-#elif defined(EX_MAC)
-
-#endif
-
-
-/*
-	debug information
-*/
-#if defined(EX_DEBUG) || (EX_ENGINE_VERSION_MAJOR < 1)
-	#if defined(EX_VC) || defined(EX_WINDOWS)
-
-	#elif defined(EX_LINUX)
-	mi = mallinfo();
-	printf("Total non-mmapped bytes (arena):       %d\n", mi.arena);
-	printf("# of free chunks (ordblks):            %d\n", mi.ordblks);
-	printf("# of free fastbin blocks (smblks):     %d\n", mi.smblks);
-	printf("# of mapped regions (hblks):           %d\n", mi.hblks);
-	printf("Bytes in mapped regions (hblkhd):      %d\n", mi.hblkhd);
-	printf("Max. total allocated space (usmblks):  %d\n", mi.usmblks);
-	printf("Free bytes held in fastbins (fsmblks): %d\n", mi.fsmblks);
-	printf("Total allocated space (uordblks):      %d\n", mi.uordblks);
-	printf("Total free space (fordblks):           %d\n", mi.fordblks);
-	printf("Topmost releasable block (keepcost):   %d\n", mi.keepcost);
-
-	muntrace();
-	#endif
-
-#endif
-
-	/**/
-	engineflag |= ELT_DEINIT;
-	engineflag = engineflag & ~ELT_INIT_EVERYTHING;
-
-	//fclose(m_file_log);
-}
 
 
 
@@ -446,7 +142,7 @@ void ExDisable(Enum disable){
 #endif
 	switch(disable){
 	case EX_CRASH_TERMINATE:
-		signal(SIGTERM,NULL);
+		signal(SIGTERM, NULL);
 		break;
 	case EX_CRASH_ABORT:
 		break;
@@ -461,6 +157,7 @@ void ExDisable(Enum disable){
 	default:return;
 	}
 }
+
 
 
 #define EX_COMPILER_VERSION(major, minor, revision) EX_TEXT("ELT-")STR(major)EX_TEXT(".")STR(minor)EX_TEXT(".")STR(revision)
---
+++ src/system/unix/unix_elt.c
@@ -0,0 +1,204 @@
+#include"elt.h"
+#include"elt_def.h"
+#include"system/elt_cl.h"
+#include"system/elt_gl.h"
+#include"system/elt_errorhandler.h"
+#include"system/elt_log.h"
+#include"system/elt_audio.h"
+
+#include"system/unix/unix_win.h"
+#include<X11/Xlib.h>
+#include<malloc.h>
+
+#include<signal.h>
+#include<mcheck.h>
+
+/*
+ *	High accuracy timer.
+ */
+extern Uint64 eltTickTime;
+
+/*
+ *
+ */
+unsigned long int engineflag = 0;
+#define ELT_DEINIT ((unsigned long int)(-1))
+
+/*
+ *
+ */
+void* xcbConnection;
+
+
+ERESULT ExInit(Enum engineFlag){
+	ERESULT result = E_OK;
+	ExHandle hmodule;
+	Int32 hConHandle;
+	Long lStdHandle;
+
+
+	/*	if all is already initilated return !	*/
+    if(engineflag & ELT_INIT_EVERYTHING){
+        return 2;
+    }
+	engineflag = engineflag & ~ELT_DEINIT;
+
+	mtrace();
+
+
+	/*	enable loggint */
+	m_file_log = fopen("EngineExDevLog.txt", "w+" );
+	if(dup2(stdout,m_file_log) == -1)
+        fprintf(stderr,"error");
+    dup2(stdout,stderr);  //redirects stderr to stdout below this line.
+
+
+    /*	Create Connection with Display Server.	*/
+    if(display){
+    	display = XOpenDisplay(getenv("DISPLAY"));
+    }
+    if(!display){
+        ExError("couldn't open Display\n");
+        abort();
+    }
+
+	/*	Initialize sub system	*/
+	ExInitSubSystem(engineFlag);
+
+
+	/*	initialize error handler.	*/
+	if(engineFlag & ELT_INIT_DEBUG){
+		if(!(result = ExInitErrorHandler())){
+			ExError(EX_TEXT("Failed to initialize error handler."));
+		}
+	}
+
+	engineflag |= engineFlag;
+
+	/* release resources even if application exit unexpected or exit without calling ExShutDown */
+	atexit(ExShutDown);
+
+	return result;
+}
+
+
+
+
+ERESULT ExInitSubSystem(Uint32 engineflag){
+	ERESULT hr = 0;
+	ExHandle hmodule;
+
+	/**/
+	engineflag = engineflag & ~ELT_DEINIT;
+
+
+	if(ELT_INIT_VIDEO & engineflag){
+
+	}
+	if(ELT_INIT_JOYSTICK & engineflag){
+
+
+	}
+	if(ELT_INIT_AUDIO & engineflag){
+		//ExAudioInit(0);
+
+
+	}
+	if(ELT_INIT_GAMECONTROLLER & engineflag){
+
+	}
+	if(ELT_INIT_EVENTS & engineflag){
+	    /*		enable X events	*/
+		XAllowEvents(display , SyncBoth,CurrentTime);
+	}
+	if(ELT_INIT_TIMER & engineflag){
+		eltTickTime = ExCurrentTime();
+	}
+	if(ELT_INIT_NET & engineflag){
+        ExLoadLibrary("");
+
+	}
+	return hr;
+}
+
+
+
+void ExQuitSubSytem(Uint32 engineflag){
+
+	if(ELT_INIT_TIMER & engineflag){
+
+	}
+	if(ELT_INIT_AUDIO & engineflag){
+		//ExUnLoadObject(ExGetFileModule(EX_TEXT("libasound.so")));
+	}
+	if(ELT_INIT_JOYSTICK & engineflag){
+        ExJoyStickClose(0);
+        ExJoyStickClose(1);
+        ExJoyStickClose(2);
+        ExJoyStickClose(3);
+		//ExJoyStickShutDown();
+	}
+	if(ELT_INIT_GAMECONTROLLER & engineflag){
+
+	}
+	if(ELT_INIT_NET & engineflag){
+
+	}
+
+	if(ELT_INIT_VIDEO & engineflag){
+
+	}
+	if(ELT_INIT_NET & engineflag){
+        ExLoadLibrary("");
+	}
+
+}
+
+
+
+void ExShutDown(void){
+    struct mallinfo mi;
+	if(engineflag & ELT_DEINIT)
+		return;
+
+	ExQuitSubSytem(ELT_INIT_EVERYTHING);
+
+	if(ExGetCurrentOpenGLContext())
+		ExDestroyGLContext(ExGetCurrentGLDrawable(), ExGetCurrentOpenGLContext());
+
+#if !(defined(EX_ANDROID) || defined(DONT_SUPPORT_OPENCL))
+	ExDestroyCLContext(ExGetCurrentCLContext());
+#endif
+
+	if(eglGetCurrentDisplay())
+		eglTerminate(eglGetCurrentDisplay());
+
+	if(xcbConnection)
+		XCloseDisplay(xcbConnection);
+	XSync(display,True);
+	if(display)
+		XFlush(display);
+    if(display){
+    	XCloseDisplay(display);
+    	display = NULL;
+    }
+	mi = mallinfo();
+	printf("Total non-mmapped bytes (arena):       %d\n", mi.arena);
+	printf("# of free chunks (ordblks):            %d\n", mi.ordblks);
+	printf("# of free fastbin blocks (smblks):     %d\n", mi.smblks);
+	printf("# of mapped regions (hblks):           %d\n", mi.hblks);
+	printf("Bytes in mapped regions (hblkhd):      %d\n", mi.hblkhd);
+	printf("Max. total allocated space (usmblks):  %d\n", mi.usmblks);
+	printf("Free bytes held in fastbins (fsmblks): %d\n", mi.fsmblks);
+	printf("Total allocated space (uordblks):      %d\n", mi.uordblks);
+	printf("Total free space (fordblks):           %d\n", mi.fordblks);
+	printf("Topmost releasable block (keepcost):   %d\n", mi.keepcost);
+
+	muntrace();
+
+	/**/
+	engineflag |= ELT_DEINIT;
+	engineflag = engineflag & ~ELT_INIT_EVERYTHING;
+
+	//fclose(m_file_log);
+}
