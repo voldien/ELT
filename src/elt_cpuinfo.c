12f2f9b1fe5dc72e26ea710297d174909988a47e
Voldie <voldemor2009@hotmail.com>
2016-06-02 20:30:30 +0200

    Minor bug fix.

--- src/elt_cpuinfo.c
+++ src/elt_cpuinfo.c
@@ -81,9 +81,8 @@ const ExChar* ExGetCPUName(void){
     int i = 0;
     int a,b,c,d;
 //https://github.com/soreau/SDL/blob/master/src/cpuinfo/SDL_cpuinfo.c
-
+#if !defined(EX_ARM)
     if(cpu_name[0] == NULL){
-    #if !defined(EX_ARM)
 		cpuid2(0x80000000,a,b,c,d);
 		if(a >= 0x80000004){
 			cpuid2(0x80000002, a, b, c, d);
