b61b910bb4a314d467240adbf2daae5dee7400d8
Voldie <voldemor2009@hotmail.com>
2016-07-13 20:25:12 +0200

    Improved macro conditions.

--- src/elt_endian.c
+++ src/elt_endian.c
@@ -3,13 +3,13 @@
 /*	TODO perhaps relocate to the header.	*/
 
 void ExSwap16(Uint16 x){
-#ifdef EX_CPU_X86_64
+#ifdef EX_X86
 	EX_ASM("xchg %b0,%h0": "=q"(x):"0"(x));
 #endif
 }
 
 void ExSwap32(Uint32 x){
-#ifdef EX_CPU_X86_64
+#ifdef EX_X86
 	EX_ASM("BSWAP %0": "=r"(x):"0"(x));
 #endif
 }
