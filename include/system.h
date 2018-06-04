85f5a5d614fdc5c278c0ba1544bf352911f3e12b
Voldie <voldiekami@gmail.com>
2018-06-04 11:33:49 +0200

    Added additional include.

--- include/system.h
+++ include/system.h
@@ -29,6 +29,7 @@
 #include"system/elt_cl.h"
 #include"system/elt_egl.h"
 #include"system/elt_net.h"
+#include"system/elt_zip.h"
 #include"system/elt_power.h"
 #include"system/elt_event.h"
 #include"system/elt_surface.h"
